623998aec6ca0289eaac80a42b99c09127d1d74f
daryagolubeva <darya.golubewa@gmail.com>
2018-10-11 19:22:26 +0300

    two functions are done;

--- word_search.js
+++ word_search.js
@@ -1,12 +1,74 @@
-function searchStraightLineInclude(word, puzzle){
+let puzzle = [
+  ["a", "k", "f", "o", "x", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "x", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]
+]
 
-}
+let newPuzzle =  puzzle[0].map((x,i) => puzzle.map(x => x[i]))
 
-function searchSnakingInclude(word, puzzle){
+//function searchStraightLineInclude(word, puzzle){
 
-}
-
-
-module.exports = {
 	
-}
+	function horSearch(word, puzzle) {
+		let str = '';
+		let newWord = word.length;
+		
+
+		for(let row = 0; row < puzzle.length; row++) {
+			for(let j = 0; j < (puzzle[row].length - newWord + 1); j++) {
+				str = puzzle[row].slice(j, j+newWord).join('');
+
+				if (str == word) {
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+
+	function verSearch(word, newPuzzle)
+	{
+		let str = '';
+		let newWord = word.length;
+		
+
+		for(let row = 0; row < newPuzzle.length; row++) {
+			for(let j = 0; j < (newPuzzle[row].length - newWord + 1); j++) {
+				str = newPuzzle[row].slice(j, j+newWord).join('');
+
+				if (str == word) {
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+
+	console.log(verSearch('ios', newPuzzle))
+
+	function diagSearch(word, puzzle) {
+		let str = '';
+		let newWord = word.length;
+		
+
+		for(let row = 0; row < puzzle.length; row--) {
+			for(let j = 0; j < (puzzle[row].length - newWord); j++) {
+				str = puzzle[row].slice(j, j+newWord).join('');
+
+				if (str == word) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	//}
+	console.log(diagSearch('bis', puzzle));
+
+//console.log(searchStraightLineInclude('fox', puzzle));
